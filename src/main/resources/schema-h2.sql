b7cda57117303e259e972ba547924d56e01e2d57
asconem <asconem@gmail.com>
2020-03-29 15:51:47 -0400

    Completed queries as directed by the exercise.

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -34,7 +34,27 @@ CREATE TABLE movies (
   rating VARCHAR2(10)
 );
 
--- Tables for in-class example
+INSERT INTO movies (
+    title, runtime, genre, imdb_score, rating)
+VALUES ('Howard the Duck', 110, 'Sci-Fi', 4.6, 'PG');
+INSERT INTO movies (
+    title, runtime, genre, imdb_score, rating)
+VALUES ('Lavalantula', 83, 'Horror', 4.7, 'TV-14');
+INSERT INTO movies (
+    title, runtime, genre, imdb_score, rating)
+VALUES ('Starship Troopers', 129, 'Sci-Fi', 7.2, 'PG-13');
+INSERT INTO movies (
+    title, runtime, genre, imdb_score, rating)
+VALUES ('Waltz With Bashir', 90, 'Documentary', 8.0, 'R');
+INSERT INTO movies (
+    title, runtime, genre, imdb_score, rating)
+VALUES ('Spaceballs', 96, 'Comedy', 7.1, 'PG');
+INSERT INTO movies (
+    title, runtime, genre, imdb_score, rating)
+VALUES ('Monsters Inc.', 92, 'Animation', 8.1, 'G');
+INSERT INTO movies (
+    title, runtime, genre, imdb_score, rating)
+VALUES ('Office Space', 89, 'Comedy', 7.9, 'R');
 
 DROP TABLE IF EXISTS cars;
 
